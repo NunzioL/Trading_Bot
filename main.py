4f1a5f4e5eceab3d32620e5113ef3e0b29a10637
Nunzio Lanza <lanza.nunzio@gmail.com>
2023-03-24 21:34:13 -0400

    Test

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
 """This program is a trading bot that uses the QuantConnect API to trade the SPY ETF. This program also uses TensorFlow to adapt its trading strategy to the market."""
-
-print("Hello World!")
